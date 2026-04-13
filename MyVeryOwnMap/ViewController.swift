f1dbb9b2b44643caa0aa47d32fa13abaf0642ccf
Banan_1984 <niketozzz@gmail.com>
2026-04-13 20:41:25 +0300

    create mapView

--- MyVeryOwnMap/ViewController.swift
+++ MyVeryOwnMap/ViewController.swift
@@ -1,9 +1,42 @@
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+final class CollectionViewController: UIViewController {
+    
+    private let mapView = MKMapView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .blue
+        setupView()
     }
 }
 
+// MARK: - Setup View
+extension CollectionViewController: MKMapViewDelegate {
+    func setupView() {
+        view.addSubview(mapView)
+        setupMapView()
+        setupLayout()
+    }
+    
+    func setupMapView() {
+        //mapView.region = MKCoordinateRegion()
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+    }
+}
+
+
+extension CollectionViewController {
+    func setupLayout() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
